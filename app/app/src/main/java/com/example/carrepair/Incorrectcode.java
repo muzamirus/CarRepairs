f5920506c7e05dc03e46933bb6ce67c0019e0a12
muzamirus <sebulimekeith007@gmail.com>
2019-06-17 11:17:31 +0300

    these are the additional changes

---
+++ app/app/src/main/java/com/example/carrepair/Incorrectcode.java
@@ -0,0 +1,13 @@
+package com.example.carrepair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Incorrectcode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incorrectcode);
+    }
+}
